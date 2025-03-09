a1387c96f98438a120fba01119efa528bd0fdb88
iamsarbartha <sarbartha@utexas.edu>
2025-03-08 20:24:59 -0600

    Added timing for each simulation step

--- run_ramulator_mnk.sh
+++ run_ramulator_mnk.sh
@@ -4,5 +4,5 @@ time python3 scalesim/scale.py -c ./configs/google.cfg -t ${topology} -i gemm > 
 time python3 scripts/dram_sim.py -topology $1 -run_name GoogleTPU_v1_os
 time python3 scripts/dram_latency.py -topology $1
 time python3 scalesim/scale.py -c ./configs/google_ramulator_${2}.cfg -t ${topology} -i gemm > ${1}_${2}_stall_out
-cp ${1}_${2}_orig_out ./results/dram_results/stall_cycles/${1}_${2}_orig_out_ws
-cp ${1}_${2}_stall_out ./results/dram_results/stall_cycles/${1}_${2}_stall_out_ws
+cp ${1}_${2}_orig_out ./results/dram_results/stall_cycles/${1}_${2}_orig_out
+cp ${1}_${2}_stall_out ./results/dram_results/stall_cycles/${1}_${2}_stall_out
